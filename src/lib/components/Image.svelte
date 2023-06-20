d830733629d648181d497810a8cb1d32711ab84f
Gabriel Uhlíř <69206870+gauel@users.noreply.github.com>
2023-06-20 09:13:34 +0200

    feat(Image): better component

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -4,9 +4,13 @@
 	export let src = "",
 	alt = "",
 	lazy = true,
-	style = "",
+	style = "relative",
+	styleImg = "",
+	styleAttr = "",
 	rounded = "rounded-xl",
-	fadeDuration = "duration-300";
+	fadeDuration = "duration-300",
+	ghost = false,
+	id = ""
 
 	let loaded = false;
 	let failed = false;
@@ -27,10 +31,10 @@
 	})
 </script>
 
-<div class="{style} relative inline-block">
-	<img {src} {alt} loading={lazy ? "lazy" : "eager"} class="w-full block transition-opacity ease-in-out {fadeDuration} {loaded ? "opacity-100" : "opacity-0"}" />
-	{#if loading}
-		<div class="{rounded} bg-white/20 animate-pulse absolute left-0 top-0 w-full h-full" transition:fade={{duration: 300}} />
+<div class="{style} inline-block" style={styleAttr} {id}>
+	<img {src} {alt} loading={lazy ? "lazy" : "eager"} class="w-full h-full object-cover {styleImg} block transition-opacity ease-out {fadeDuration} {loaded ? "opacity-100" : "opacity-0"}" />
+	{#if (loading && ghost)}
+		<div class="{rounded} bg-white/30 animate-pulse absolute left-0 top-0 w-full h-full" transition:fade={{duration: 300}} />
 	{:else if failed}
 		<div class="{rounded} bg-red-500/30 border border-red-500/60 flex items-center justify-center absolute left-0 top-0 w-full h-full" transition:fade={{duration: 300}}>Image failed to load</div>
 	{/if}
