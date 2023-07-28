db8d88721a6e7b644e12cf25509028a506b22218
Gabriel Uhlíř <69206870+gauel@users.noreply.github.com>
2023-07-28 10:15:55 +0200

    feat(Home): welcome message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import Fa from 'svelte-fa'
-	import { faFlag } from '@fortawesome/free-solid-svg-icons'
+	import { faCertificate } from '@fortawesome/free-solid-svg-icons'
 </script>
-<div class="text-white">
-	New app
-	<Fa icon={faFlag} />
+<div class="text-white w-full h-screen flex flex-col items-center justify-center gap-4 ">
+	<Fa icon={faCertificate} class="text-[64px] text-amber-500 animate-spin" />
+	<h1 class="text-clamp-xs font-bold uppercase">Svelte starter</h1>
+	<p class="opacity-50">With our custom features</p>
 </div>
