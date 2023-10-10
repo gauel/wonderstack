0d3410bda694f3131a5f202f6dccaa30319064ad
Gabriel Uhlíř <69206870+gauel@users.noreply.github.com>
2023-10-10 10:55:02 +0200

    feat(Config): adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter({})
 	},
 	preprocess: vitePreprocess()
 };
