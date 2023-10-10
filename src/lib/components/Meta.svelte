1d8260d035e7dfe1641791245b0fe6b2fb551938
Gabriel Uhlíř <69206870+gauel@users.noreply.github.com>
2023-10-10 10:54:54 +0200

    fix(Meta): kicker

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -1,44 +1,41 @@
 <script>
-
-	import shareImageFile from "$lib/assets/shareimage.jpg";
-	import favIcon from "$lib/assets/favicon.png";
-	import touchIcon from "$lib/assets/touchicon.png"
+	/* Default values */
+	let kicker = 'Kicker — ',
+		title = 'Title',
+		description = 'Description',
+		shareImage = '/shareimage.jpg',
+		themeColor = '#000';
 
 	export let meta = {
-		kicker: "",
-		title: "",
-		description: "",
-		shareImage: "",
-		themeColor: ""
-	}
-
-	/* Default values */
-	let kicker = "Website — ",
-		title = "Page title",
-		description = "Description",
-		shareImage = shareImageFile,
-		themeColor = "#990000"
+		title: '',
+		description: '',
+		shareImage: '',
+		themeColor: ''
+	};
 </script>
 
 <svelte:head>
 	<meta charset="utf-8" />
 	<meta http-equiv="Content-Language" content="cs" />
-	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta
+		name="viewport"
+		content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+	/>
 
-	<title>{meta.kicker || kicker} {meta.title || title}</title>
-	<meta name="description" content={meta.description || description || description} />
+	<title>{kicker}{meta?.title || title}</title>
+	<meta name="description" content={meta?.description || description} />
 
-	<meta property="og:title" content={meta.title || title} />
-	<meta property="og:description" content={meta.description || description} />
-	<meta property="og:image" content={meta.shareImage || shareImage} />
+	<meta property="og:title" content={meta?.title || title} />
+	<meta property="og:description" content={meta?.description || description} />
+	<meta property="og:image" content={meta?.shareImage || shareImage} />
 	<meta property="og:type" content="website" />
 
 	<meta property="twitter:title" content="" />
-	<meta property="twitter:description" content={meta.description || description} />
-	<meta property="twitter:image" content={meta.shareImage || shareImage} />
+	<meta property="twitter:description" content={meta?.description || description} />
+	<meta property="twitter:image" content={meta?.shareImage || shareImage} />
 	<meta name="twitter:card" content="summary_large_image" />
 
-	<meta name="theme-color" content={meta.themeColor || themeColor} />
-	<link href={favIcon} rel="shortcut icon" type="image/x-icon" />
-	<link href={touchIcon} rel="apple-touch-icon" />
+	<meta name="theme-color" content={meta?.themeColor || themeColor} />
+	<link href="/favicon.png" rel="shortcut icon" type="image/x-icon" />
+	<link href="/touchicon.png" rel="apple-touch-icon" />
 </svelte:head>
