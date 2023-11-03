0cd8d6065d44823ebeb8448a205f0ccb277e6a91
Gabriel Uhlíř <g.uhlir@gmail.com>
2023-11-04 00:12:00 +0100

    feat(Global): better libraries

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { imagetools } from '@zerodevx/svelte-img/vite'
-
-const supportedExtensions = ['png', 'jpg', 'jpeg'];
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
-		imagetools({
-			defaultDirectives: () => new URLSearchParams('?run&width=500;800;1280;2200&format=webp;jpg')
-		}),
 		sveltekit()
 	]
 };
